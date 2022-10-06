273ba80e1010bb8efacf7d900ab70f26d6f26aa3
Nikita Nemykin <nikitanemykin@mail.ru>
2022-10-06 15:45:39 +0700

    Add navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
 import './Navigation.scss';
 
 export const Navigation = () => {
-  return <nav className="navigation"></nav>;
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <div className="navigation">
+      <input
+        checked={isChecked}
+        onChange={e => setIsChecked(e.target.checked)}
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+      />
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+      <div className="navigation__background">&nbsp;</div>
+
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span className="navigation__count">01 &nbsp;</span>About Natours
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span className="navigation__count">02 &nbsp;</span>Your benefits
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span className="navigation__count">03 &nbsp;</span>Popular tours
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span className="navigation__count">04 &nbsp;</span>Stories
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span className="navigation__count">05 &nbsp;</span>Book now
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
