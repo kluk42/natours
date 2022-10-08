f811f24abfc38d0993cc635593e06c32d33fa11c
Nikita Nemykin <nikitanemykin@mail.ru>
2022-10-08 16:13:42 +0700

    Add responsive images

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
-import logoImgPath from '../../assets/logo-green-2x.png';
+import logoImgPathLargeLowDensity from '../../assets/logo-green-1x.png';
+import logoImgPathLargeHighDensity from '../../assets/logo-green-2x.png';
+import logoImgPathPhoneLowDensity from '../../assets/logo-green-small-1x.png';
+import logoImgPathPhoneHighDensity from '../../assets/logo-green-small-2x.png';
 import { FloatColumn } from '../../layouts/FloatGrid/FloatColumn';
 import { FloatRow } from '../../layouts/FloatGrid/FloatRow';
 
@@ -8,7 +11,16 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__logo-box">
-        <img src={logoImgPath} alt="Full logo" className="footer__logo" />
+        <picture className="footer__logo">
+          <source
+            srcSet={`${logoImgPathPhoneLowDensity} 1x, ${logoImgPathPhoneHighDensity} 2x`}
+            media="(max-width: 37.5em)"
+          />
+          <img
+            srcSet={`${logoImgPathLargeLowDensity} 1x, ${logoImgPathLargeHighDensity} 2x`}
+            alt="Full logo"
+          />
+        </picture>
       </div>
       <FloatRow>
         <FloatColumn className="col-1-of-2">
--- src/components/NatoursAbout/Composition.tsx
+++ src/components/NatoursAbout/Composition.tsx
@@ -1,15 +1,36 @@
-import firstImgPath from '../../assets/nat-1-large.jpg';
-import secondImgPath from '../../assets/nat-2-large.jpg';
-import thirdImgPath from '../../assets/nat-3-large.jpg';
+import firstLargeImgPath from '../../assets/nat-1-large.jpg';
+import firstSmallImagePath from '../../assets/nat-1.jpg';
+import secondImgLargePath from '../../assets/nat-2-large.jpg';
+import secondImgSmallPath from '../../assets/nat-2.jpg';
+import thirdImgLargePath from '../../assets/nat-3-large.jpg';
+import thirdImgSmallPath from '../../assets/nat-3.jpg';
 
 import './Composition.scss';
 
 export const Composition = () => {
   return (
     <div className="composition">
-      <img src={firstImgPath} alt="Photo 1" className="composition__photo composition__photo_p1" />
-      <img src={secondImgPath} alt="Photo 2" className="composition__photo composition__photo_p2" />
-      <img src={thirdImgPath} alt="Photo 3" className="composition__photo composition__photo_p3" />
+      <img
+        alt="Photo 1"
+        src={firstLargeImgPath}
+        srcSet={`${firstSmallImagePath} 300w, ${firstLargeImgPath} 1000w`}
+        sizes="(max-width: 900px) 20vw, (max-widht: 600px) 30vw, 300px)"
+        className="composition__photo composition__photo_p1"
+      />
+      <img
+        alt="Photo 2"
+        src={secondImgSmallPath}
+        srcSet={`${secondImgSmallPath} 300w, ${secondImgLargePath} 1000w`}
+        sizes="(max-width: 900px) 20vw, (max-widht: 600px) 30vw, 300px)"
+        className="composition__photo composition__photo_p2"
+      />
+      <img
+        alt="Photo 3"
+        src={thirdImgSmallPath}
+        srcSet={`${thirdImgSmallPath} 300w, ${thirdImgLargePath} 1000w`}
+        sizes="(max-width: 900px) 20vw, (max-widht: 600px) 30vw, 300px)"
+        className="composition__photo composition__photo_p3"
+      />
     </div>
   );
 };
